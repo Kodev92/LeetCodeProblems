ec14e827751081ad5f38fa696434d4ad680b0034
Kodev92 <kodnh5@gmail.com>
2022-01-23 18:41:38 -0600

    283 "Move Zeroes" solved

---
+++ src/leetcode/_283_MoveZeroes.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class _283_MoveZeroes {
+    public void moveZeroes(int[] nums) {
+
+        // Running from 0 to length - 1 element, it is pointer A
+        // pointer B running from pointer A + 1 to the end
+        // if it is 0 at pointer B then  pointer B ++ else
+        // nums[pointer A] = nums[pointer B]
+        // if poinetr B at the end, then nums[pointer A] = 0
+
+        int b = 0;
+        for (int a = 0; a < nums.length; a++) {
+            while (b < nums.length && nums[b] == 0) b++;
+            if (b < nums.length) nums[a] = nums[b];
+                else nums[a] = 0;
+            b++;
+        }
+
+    }
+}
